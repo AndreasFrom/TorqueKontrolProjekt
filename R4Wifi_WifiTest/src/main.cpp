b1162c29ec041adca7f1ec7e516f3c3b9c076a3f
AndreasFrom <minifrom@gmail.com>
2025-02-21 13:41:12 +0100

    Simple working udp client and server

--- R4Wifi_WifiTest/src/main.cpp
+++ R4Wifi_WifiTest/src/main.cpp
@@ -1,183 +1,93 @@
-/*
-  Udp NTP Client
-
-  Get the time from a Network Time Protocol (NTP) time server
-  Demonstrates use of UDP sendPacket and ReceivePacket
-  For more on NTP time servers and the messages needed to communicate with them,
-  see http://en.wikipedia.org/wiki/Network_Time_Protocol
-
-  created 4 Sep 2010
-  by Michael Margolis
-  modified 9 Apr 2012
-  by Tom Igoe
-  modified May, 4th 2023
-  by Daniele Aimo
-
-  This code is in the public domain.
-
-  Find the full UNO R4 WiFi Network documentation here:
-  https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#wi-fi-udp-ntp-client
- */
-
+#include <WiFi.h>
+#include <WiFiUdp.h>
 #include <Arduino.h>
- #include <WiFiS3.h>
 
-#define SECRET_SSID "coolguys123"
-#define SECRET_PASS "werty123"
+/* WiFi network name and password */
+const char* ssid = "coolguys123";
+const char* pwd = "werty123";
 
- int status = WL_IDLE_STATUS;
+// IP address of your PC running the Python script
+const char* udpAddress = "192.168.93.49";  // Updated to your actual PC IP
+const int udpPort = 4242;
 
- ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
- char ssid[] = SECRET_SSID;        // your network SSID (name)
- char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
- int keyIndex = 0;            // your network key index number (needed only for WEP)
- 
- unsigned int localPort = 2390;      // local port to listen for UDP packets
- 
- IPAddress timeServer(162, 159, 200, 123); // pool.ntp.org NTP server
- 
- const int NTP_PACKET_SIZE = 48; // NTP timestamp is in the first 48 bytes of the message
- 
- byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
- 
- // A UDP instance to let us send and receive packets over UDP
- WiFiUDP Udp;
- 
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define LSM6DSO_ADDRESS 0xD5
 
- void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+#define DEV_I2C Wire
 
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+// Create UDP instance
+WiFiUDP udp;
 
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+void setup() {
+    Serial.begin(115200);
+    while (!Serial); // Wait for Serial to be ready
+
+    // Connect to WiFi
+    WiFi.begin(ssid, pwd);
+    Serial.print("Connecting to WiFi...");
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("\nConnected to WiFi!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    // Start UDP
+    udp.begin(udpPort);
 }
 
+void loop() {
+    int32_t accelerometer[3] = {0, 1, 2};  // Replace with actual accelerometer data
 
- // send an NTP request to the time server at the given address
- unsigned long sendNTPpacket(IPAddress& address) {
-  //Serial.println("1");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  //Serial.println("2");
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
+    char data_string[50];
+    snprintf(data_string, sizeof(data_string), "%ld,%ld,%ld", 
+             accelerometer[0], accelerometer[1], accelerometer[2]);
 
-  //Serial.println("3");
+    bool ack_received = false;
+    int retry_count = 0;
+    const int max_retries = 3;
 
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  //Serial.println("4");
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  //Serial.println("5");
-  Udp.endPacket();
-  //Serial.println("6");
+    while (!ack_received && retry_count < max_retries) {
+        Serial.print("Sending data: ");
+        Serial.println(data_string);
+
+        // Send UDP packet
+        udp.beginPacket(udpAddress, udpPort);
+        udp.write(data_string);
+        udp.endPacket();
+
+        unsigned long start_time = millis();
+        char ack_buffer[10];
+
+        // Wait for ACK (1-second timeout)
+        while (millis() - start_time < 1000) {
+            int packet_size = udp.parsePacket();
+            if (packet_size) {
+                int len = udp.read(ack_buffer, sizeof(ack_buffer) - 1);
+                if (len > 0) {
+                    ack_buffer[len] = '\0';
+                    if (strcmp(ack_buffer, "ACK") == 0) {
+                        ack_received = true;
+                        Serial.println("ACK received!");
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (!ack_received) {
+            retry_count++;
+            Serial.println("ACK not received, retrying...");
+        }
+    }
+
+    if (!ack_received) {
+        Serial.println("Failed to receive ACK after multiple attempts.");
+    }
+
+    delay(1000);  // Wait before sending the next packet
 }
-
-
-
- void setup() {
-   // Open serial communications and wait for port to open:
-   Serial.begin(115200);
-   while (!Serial) {
-     ; // wait for serial port to connect. Needed for native USB port only
-   }
-   
-   // check for the WiFi module:
-   if (WiFi.status() == WL_NO_MODULE) {
-     Serial.println("Communication with WiFi module failed!");
-     // don't continue
-     while (true);
-   }
- 
-   String fv = WiFi.firmwareVersion();
-   if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-     Serial.println("Please upgrade the firmware");
-   }
- 
-   // attempt to connect to WiFi network:
-   while (status != WL_CONNECTED) {
-     Serial.print("Attempting to connect to SSID: ");
-     Serial.println(ssid);
-     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-     status = WiFi.begin(ssid, pass);
- 
-     // wait 10 seconds for connection:
-     delay(10000);
-   }
- 
-   Serial.println("Connected to WiFi");
-   printWifiStatus();
- 
-   Serial.println("\nStarting connection to server...");
-   Udp.begin(localPort);
- }
- 
- void loop() {
-   sendNTPpacket(timeServer); // send an NTP packet to a time server
-   // wait to see if a reply is available
-   delay(1000);
-   if (Udp.parsePacket()) {
-     Serial.println("packet received");
-     // We've received a packet, read the data from it
-     Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
- 
-     //the timestamp starts at byte 40 of the received packet and is four bytes,
-     // or two words, long. First, extract the two words:
- 
-     unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-     unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-     // combine the four bytes (two words) into a long integer
-     // this is NTP time (seconds since Jan 1 1900):
-     unsigned long secsSince1900 = highWord << 16 | lowWord;
-     Serial.print("Seconds since Jan 1 1900 = ");
-     Serial.println(secsSince1900);
- 
-     // now convert NTP time into everyday time:
-     Serial.print("Unix time = ");
-     // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-     const unsigned long seventyYears = 2208988800UL;
-     // subtract seventy years:
-     unsigned long epoch = secsSince1900 - seventyYears;
-     // print Unix time:
-     Serial.println(epoch);
- 
- 
-     // print the hour, minute and second:
-     Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-     Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-     Serial.print(':');
-     if (((epoch % 3600) / 60) < 10) {
-       // In the first 10 minutes of each hour, we'll want a leading '0'
-       Serial.print('0');
-     }
-     Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-     Serial.print(':');
-     if ((epoch % 60) < 10) {
-       // In the first 10 seconds of each minute, we'll want a leading '0'
-       Serial.print('0');
-     }
-     Serial.println(epoch % 60); // print the second
-   }
-   // wait ten seconds before asking for the time again
-   delay(10000);
- }
- 
--- R4Wifi_WifiTest/src/wifilogger.py
+++ R4Wifi_WifiTest/src/wifilogger.py
@@ -1,72 +1,19 @@
 import socket
-import time
-import struct
 
-# NTP server settings
-NTP_PORT = 123
-LOCAL_PORT = 2390
-NTP_SERVER = '162.159.200.123'
-NTP_PACKET_SIZE = 48
+UDP_IP = "192.168.93.49"  # Use your actual WiFi IP
+UDP_PORT = 4242  # Port to listen on
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', LOCAL_PORT))
+sock.bind((UDP_IP, UDP_PORT))
 
-print(f"Listening for UDP packets on port {LOCAL_PORT}...")
-
-def create_ntp_response():
-    # NTP packet format
-    ntp_packet = bytearray(NTP_PACKET_SIZE)
-    
-    # Set the first byte (LI, Version, Mode)
-    ntp_packet[0] = 0b00100100  # LI = 0 (no warning), Version = 4, Mode = 4 (server)
-    
-    # Set the Stratum (1 = primary server)
-    ntp_packet[1] = 1
-    
-    # Set the Polling Interval (6 = 64 seconds)
-    ntp_packet[2] = 6
-    
-    # Set the Precision (0xEC = -20 in decimal, which is 2^-20 seconds)
-    ntp_packet[3] = 0xEC
-    
-    # Set the Root Delay (0x0000)
-    ntp_packet[4:8] = struct.pack('!I', 0)
-    
-    # Set the Root Dispersion (0x0000)
-    ntp_packet[8:12] = struct.pack('!I', 0)
-    
-    # Set the Reference Identifier (0x00000000)
-    ntp_packet[12:16] = struct.pack('!I', 0)
-    
-    # Set the Reference Timestamp (current time)
-    ref_time = time.time()
-    ntp_packet[16:24] = struct.pack('!Q', int(ref_time * 2**32))
-    
-    # Set the Originate Timestamp (same as the client's transmit timestamp)
-    # This will be filled in when we receive the client's packet
-    
-    # Set the Receive Timestamp (current time)
-    recv_time = time.time()
-    ntp_packet[32:40] = struct.pack('!Q', int(recv_time * 2**32))
-    
-    # Set the Transmit Timestamp (current time)
-    transmit_time = time.time()
-    ntp_packet[40:48] = struct.pack('!Q', int(transmit_time * 2**32))
-    
-    return ntp_packet
+print(f"Listening for UDP packets on {UDP_IP}:{UDP_PORT}...")
 
 while True:
-    # Receive data from the client
-    data, addr = sock.recvfrom(NTP_PACKET_SIZE)
-    print(f"Received NTP request from {addr}")
-    
-    # Create the NTP response packet
-    ntp_response = create_ntp_response()
-    
-    # Copy the client's transmit timestamp to the originate timestamp in the response
-    ntp_response[24:32] = data[40:48]
-    
-    # Send the NTP response back to the client
-    sock.sendto(ntp_response, addr)
-    print(f"Sent NTP response to {addr}")
+    data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
+    print(f"Received message: {data.decode()} from {addr}")
+
+    # Send ACK back
+    ack_message = "ACK".encode()
+    sock.sendto(ack_message, addr)
+    print("ACK sent.")
