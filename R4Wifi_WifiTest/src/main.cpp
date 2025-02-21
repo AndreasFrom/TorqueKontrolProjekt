151c2440e6b67c2a6531d604c4f1456593845237
AndreasFrom <minifrom@gmail.com>
2025-02-21 12:36:31 +0100

    first tests with udp server

--- R4Wifi_WifiTest/src/main.cpp
+++ R4Wifi_WifiTest/src/main.cpp
@@ -1,38 +1,49 @@
-
-
 /*
-  WiFi Web Server LED Blink
+  Udp NTP Client
 
-  A simple web server that lets you blink an LED via the web.
-  This sketch will print the IP address of your WiFi module (once connected)
-  to the Serial Monitor. From there, you can open that address in a web browser
-  to turn on and off the LED_BUILTIN.
+  Get the time from a Network Time Protocol (NTP) time server
+  Demonstrates use of UDP sendPacket and ReceivePacket
+  For more on NTP time servers and the messages needed to communicate with them,
+  see http://en.wikipedia.org/wiki/Network_Time_Protocol
 
-  If the IP address of your board is yourAddress:
-  http://yourAddress/H turns the LED on
-  http://yourAddress/L turns it off
-
-  This example is written for a network using WPA encryption. For
-  WEP or WPA, change the WiFi.begin() call accordingly.
-
-  Circuit:
-  * Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and Uno WiFi Rev.2)
-  * LED attached to pin 9
-
-  created 25 Nov 2012
+  created 4 Sep 2010
+  by Michael Margolis
+  modified 9 Apr 2012
   by Tom Igoe
+  modified May, 4th 2023
+  by Daniele Aimo
+
+  This code is in the public domain.
 
   Find the full UNO R4 WiFi Network documentation here:
-  https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#simple-webserver
+  https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#wi-fi-udp-ntp-client
  */
 
- #include <Arduino.h>
- #include "WiFiS3.h"
+#include <Arduino.h>
+ #include <WiFiS3.h>
 
- #define SECRET_SSID "coolguys123"
- #define SECRET_PASS "werty123"
+#define SECRET_SSID "coolguys123"
+#define SECRET_PASS "werty123"
 
+ int status = WL_IDLE_STATUS;
+
+ ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+ char ssid[] = SECRET_SSID;        // your network SSID (name)
+ char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+ int keyIndex = 0;            // your network key index number (needed only for WEP)
  
+ unsigned int localPort = 2390;      // local port to listen for UDP packets
+ 
+ IPAddress timeServer(162, 159, 200, 123); // pool.ntp.org NTP server
+ 
+ const int NTP_PACKET_SIZE = 48; // NTP timestamp is in the first 48 bytes of the message
+ 
+ byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+ 
+ // A UDP instance to let us send and receive packets over UDP
+ WiFiUDP Udp;
+ 
+
  void printWifiStatus() {
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
@@ -48,25 +59,48 @@
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
-  // print where to go in a browser:
-  Serial.print("To see this page in action, open a browser to http://");
-  Serial.println(ip);
 }
 
 
- ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
- char ssid[] = SECRET_SSID;        // your network SSID (name)
- char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
- int keyIndex = 0;                 // your network key index number (needed only for WEP)
- 
- int led =  LED_BUILTIN;
- int status = WL_IDLE_STATUS;
- WiFiServer server(80);
- 
+ // send an NTP request to the time server at the given address
+ unsigned long sendNTPpacket(IPAddress& address) {
+  //Serial.println("1");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  //Serial.println("2");
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  //Serial.println("3");
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  //Serial.println("4");
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  //Serial.println("5");
+  Udp.endPacket();
+  //Serial.println("6");
+}
+
+
+
  void setup() {
-   Serial.begin(115200);      // initialize serial communication
-   pinMode(led, OUTPUT);      // set the LED pin mode
- 
+   // Open serial communications and wait for port to open:
+   Serial.begin(115200);
+   while (!Serial) {
+     ; // wait for serial port to connect. Needed for native USB port only
+   }
+   
    // check for the WiFi module:
    if (WiFi.status() == WL_NO_MODULE) {
      Serial.println("Communication with WiFi module failed!");
@@ -81,68 +115,69 @@
  
    // attempt to connect to WiFi network:
    while (status != WL_CONNECTED) {
-     Serial.print("Attempting to connect to Network named: ");
-     Serial.println(ssid);                   // print the network name (SSID);
- 
+     Serial.print("Attempting to connect to SSID: ");
+     Serial.println(ssid);
      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
      status = WiFi.begin(ssid, pass);
+ 
      // wait 10 seconds for connection:
      delay(10000);
    }
-   server.begin();                           // start the web server on port 80
-   printWifiStatus();                        // you're connected now, so print out the status
+ 
+   Serial.println("Connected to WiFi");
+   printWifiStatus();
+ 
+   Serial.println("\nStarting connection to server...");
+   Udp.begin(localPort);
  }
  
- 
  void loop() {
-   WiFiClient client = server.available();   // listen for incoming clients
+   sendNTPpacket(timeServer); // send an NTP packet to a time server
+   // wait to see if a reply is available
+   delay(1000);
+   if (Udp.parsePacket()) {
+     Serial.println("packet received");
+     // We've received a packet, read the data from it
+     Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
  
-   if (client) {                             // if you get a client,
-     Serial.println("new client");           // print a message out the serial port
-     String currentLine = "";                // make a String to hold incoming data from the client
-     while (client.connected()) {            // loop while the client's connected
-       if (client.available()) {             // if there's bytes to read from the client,
-         char c = client.read();             // read a byte, then
-         Serial.write(c);                    // print it out to the serial monitor
-         if (c == '\n') {                    // if the byte is a newline character
+     //the timestamp starts at byte 40 of the received packet and is four bytes,
+     // or two words, long. First, extract the two words:
  
-           // if the current line is blank, you got two newline characters in a row.
-           // that's the end of the client HTTP request, so send a response:
-           if (currentLine.length() == 0) {
-             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-             // and a content-type so the client knows what's coming, then a blank line:
-             client.println("HTTP/1.1 200 OK");
-             client.println("Content-type:text/html");
-             client.println();
+     unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+     unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+     // combine the four bytes (two words) into a long integer
+     // this is NTP time (seconds since Jan 1 1900):
+     unsigned long secsSince1900 = highWord << 16 | lowWord;
+     Serial.print("Seconds since Jan 1 1900 = ");
+     Serial.println(secsSince1900);
  
-             // the content of the HTTP response follows the header:
-             client.print("<p style=\"font-size:7vw;\">Click <a href=\"/H\">here</a> turn the LED on<br></p>");
-             client.print("<p style=\"font-size:7vw;\">Click <a href=\"/L\">here</a> turn the LED off<br></p>");
-             
-             // The HTTP response ends with another blank line:
-             client.println();
-             // break out of the while loop:
-             break;
-           } else {    // if you got a newline, then clear currentLine:
-             currentLine = "";
-           }
-         } else if (c != '\r') {  // if you got anything else but a carriage return character,
-           currentLine += c;      // add it to the end of the currentLine
-         }
+     // now convert NTP time into everyday time:
+     Serial.print("Unix time = ");
+     // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+     const unsigned long seventyYears = 2208988800UL;
+     // subtract seventy years:
+     unsigned long epoch = secsSince1900 - seventyYears;
+     // print Unix time:
+     Serial.println(epoch);
  
-         // Check to see if the client request was "GET /H" or "GET /L":
-         if (currentLine.endsWith("GET /H")) {
-           digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
-         }
-         if (currentLine.endsWith("GET /L")) {
-           digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
-         }
-       }
-       
+ 
+     // print the hour, minute and second:
+     Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+     Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+     Serial.print(':');
+     if (((epoch % 3600) / 60) < 10) {
+       // In the first 10 minutes of each hour, we'll want a leading '0'
+       Serial.print('0');
      }
-     // close the connection:
-     client.stop();
-     Serial.println("client disconnected");
+     Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+     Serial.print(':');
+     if ((epoch % 60) < 10) {
+       // In the first 10 seconds of each minute, we'll want a leading '0'
+       Serial.print('0');
+     }
+     Serial.println(epoch % 60); // print the second
    }
+   // wait ten seconds before asking for the time again
+   delay(10000);
  }
  
