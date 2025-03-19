020f8b635dea86c0065f6974db0456d7bc9cd0be
swe <swe@airborneinstruments.dk>
2025-03-19 20:20:26 +0100

    started work

--- CCU/src/i2c_master.h
+++ CCU/src/i2c_master.h
@@ -7,8 +7,8 @@
 class I2CMaster {
 public:
     I2CMaster();
-    void begin();
-    bool sendData(uint8_t slave_adress,uint8_t setpoint, uint8_t kp, uint8_t ki, uint8_t kd);
+    void begin(); 
+    bool sendData(uint8_t slave_adress, uint8_t setpoint, uint8_t kp, uint8_t ki, uint8_t kd);
     bool requestData(uint8_t slave_adress);
 
 private:
