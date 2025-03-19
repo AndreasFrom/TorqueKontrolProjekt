8ac28ecfb55d207b090072ea5cca4e6096fce082
AndreasFrom <minifrom@gmail.com>
2025-03-19 13:28:12 +0100

    Updated for correct adress

--- i2c_basic_test/simplei2c_slave/src/main.cpp
+++ i2c_basic_test/simplei2c_slave/src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 #include "i2c_slave.h"
 
-I2CSlave i2cSlave(0x42); 
+I2CSlave i2cSlave(0x08); 
 
 void setup() {
     Serial.begin(115200);
     i2cSlave.begin();
+    Serial.println("I2C Slave initialized");
 }
 
 void loop() {
     delay(1000);
+    Serial.println("Looping...");
 }
