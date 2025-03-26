2aef88801db01cb57ed7194ccad977b8a2bc54d5
swe <swe@airborneinstruments.dk>
2025-03-26 11:09:45 +0100

    changed wheel speeds, a little less than 1m turning radius

--- CCU/src/main.cpp
+++ CCU/src/main.cpp
@@ -99,10 +99,10 @@ void setup() {
     }
     Serial.println("Setup complete!");
 
-    int setpoint0 = 131;
-    int setpoint1 = 174;//(int)(205/100);
-    int setpoint2 = 131;//(int)(117/100);
-    int setpoint3 = 174;//(int)(164/100);
+    int setpoint0 = 42; // left front
+    int setpoint1 = 520; // right front
+    int setpoint2 = 42; // left rear
+    int setpoint3 = 520; // right rear
     // Temp until send from commander works
     i2cMaster.sendParam(SLAVE_ADDRESS_START, mode, kp, ki, kd);
     i2cMaster.sendSetpoint(SLAVE_ADDRESS_START, setpoint0);  
