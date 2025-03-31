1dad3a2ce171b1221bef7631a9ca493ff8522e1c
swe <swe@airborneinstruments.dk>
2025-03-31 10:48:20 +0200

    tried to go back some steps. using the hopefully working PID controller, but changed to use internal RPM control for velocity control

--- PID arduino nano speed control/src/motor_pid.h
+++ PID arduino nano speed control/src/motor_pid.h
@@ -5,13 +5,14 @@
 
 class MotorPID {
 public:
-    MotorPID(double kp, double ki, double kd, double setpoint = 300, double sampleTime = 0.01);
-    void setGains(double kp, double ki, double kd);
+    MotorPID(uint8_t mode, double kp, double ki, double kd, double setpoint = 300, double sampleTime = 0.01);
+    void setGains(uint8_t mode, double kp, double ki, double kd);
     void setSetpoint(double setpoint);
     double compute(double current_value);
     void reset();
 
 private:
+    uint8_t mode_;
     double kp_, ki_, kd_; // PID gains
     double setpoint_;     // Desired setpoint
     double integral_;     // Integral term
