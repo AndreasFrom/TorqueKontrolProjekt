1dbdaa10b12de3c6dd82c7927c07003ab8078619
AndreasFrom <minifrom@gmail.com>
2025-04-03 12:40:03 +0200

    Angular displacement

--- Python/experimental_kinematics_expanded_model/turnanalyse.py
+++ Python/experimental_kinematics_expanded_model/turnanalyse.py
@@ -19,5 +19,13 @@ M1_velocity = df["MU1value"].values * 0.068 * np.pi / 60
 displacement_0 = integrate.trapezoid(M0_velocity, time)  # Integrate M0 to get displacement
 displacement_1 = integrate.trapezoid(M1_velocity, time)  # Integrate M1 to get displacement
 
+
 print("Displacement MO in distance: " + str(displacement_0))
 print("Displacement M1 in distance: " + str(displacement_1))
+
+# Gyro_z values are in degrees/sec
+gyro_z = df["gyro_z"].values   # Convert to rad/sec
+angular_velocity = integrate.cumulative_trapezoid(gyro_z, time, initial=0)
+displacement_rotation = integrate.trapezoid(angular_velocity, time)
+
+print(f"Total angular displacement in degrees: {displacement_rotation} degrees")
