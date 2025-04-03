ccfc82191a85721dbd88b8b3cbc4e8e7db3167f4
mads jorgensen <mads-j@live.com>
2025-04-03 13:13:47 +0200

    ICO learning first draft
    header, and cpp file created in a new folder called ICO_learning

---
+++ ICO_leaning/ICO_algro.h
@@ -0,0 +1,25 @@
+#ifndef ICO_ALGRO_H
+#define ICO_ALGRO_H
+
+#include <Arduino.h>
+
+class ICOAlgro {
+public:
+    ICOAlgro(double IMU_T0, double IMU_T_1, double eta, double omega2 ,double sampleTime);
+    double computeICOError(double IMU_T0, double IMU_T_1);
+    double computeOmega1 (double IMU_T0, double IMU_T_1, double eta);
+    double computeDeltaTorque_Speed(double IMU_T0, double IMU_T_1, double eta, double omega2);
+    void resetICO();
+
+private:
+    double IMU_T0_;  // Current data from IMU
+    double IMU_T_1_; // Previous data from IMU
+    double eta_;     // Learning rate
+    double omega2_ = 1;   // Scale for error
+    double error_; // Error used in ICO
+    double prev_error_ = 1;   // Previous error for derivative term
+    double sampleTime_;   // Sample time for ICO computation
+    double omega1_ = 0; // learning 
+};
+
+#endif  // ICO_ALGRO_H
